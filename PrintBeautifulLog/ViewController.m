64b4f67d3c7ce55407b8aba6a39d9751b0f00595
shixueqian <shixueqian12345@163.com>
2018-09-14 17:58:11 +0800

    修改demo代码

--- PrintBeautifulLog/ViewController.m
+++ PrintBeautifulLog/ViewController.m
@@ -24,41 +24,31 @@
 
 - (IBAction)printDict:(id)sender {
     
-    
+    NSArray *array = @[
+                       @"张三",
+                       @"李四",
+                       @"王五"
+                       ];
+    NSLog(@"打印出的数组：%@",array);
     
     
     NSDictionary *dict = @{
                            @"ArticleTitle":@"【iOS开发】打开另一个APP（URL Scheme与openURL）",
+                           @"ArticleUrl":@"https://www.jianshu.com/p/0811ccd6a65d",
+                           @"author": @{
+                                        @"nickName":@"谦言忘语",
+                                        @"blog":@"https://www.jianshu.com/u/cc2cf725ac0c",
+                                        @"work":@"iOS工程师"
+                                        },
+                           @"friends":array
                            };
-    NSLog(@"aaaaa");
     NSLog(@"打印出的字典：%@",dict);
     
     
-//    NSArray *array = @[
-//                       @"张三",
-//                       @"李四",
-//                       @"王五"
-//                       ];
-//    NSLog(@"打印出的数组：%@",array);
-    
-    
-//    NSDictionary *dict = @{
-//                           @"ArticleTitle":@"【iOS开发】打开另一个APP（URL Scheme与openURL）",
-//                           @"ArticleUrl":@"https://www.jianshu.com/p/0811ccd6a65d",
-//                           @"author": @{
-//                                        @"nickName":@"谦言忘语",
-//                                        @"blog":@"https://www.jianshu.com/u/cc2cf725ac0c",
-//                                        @"work":@"iOS工程师"
-//                                        },
-//                           @"friends":array
-//                           };
-//    NSLog(@"打印出的字典：%@",dict);
-    
-    
-//    array = nil;
-//    dict = nil;
-//    NSLog(@"打印出的数组：%@",array);
-//    NSLog(@"打印出的字典：%@",dict);
+    array = nil;
+    dict = nil;
+    NSLog(@"打印出的数组：%@",array);
+    NSLog(@"打印出的字典：%@",dict);
 }
 
 - (IBAction)printModelArray:(id)sender {
